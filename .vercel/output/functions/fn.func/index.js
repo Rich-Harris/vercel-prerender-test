53a0cc8738dd630d173fc4a09f8289212e9dc238
Rich Harris <hello@rich-harris.dev>
2023-02-06 15:45:48 -0500

    try this

--- .vercel/output/functions/fn.func/index.js
+++ .vercel/output/functions/fn.func/index.js
@@ -1,4 +1,6 @@
 export default async (req, res) => {
+	const url = req.headers['x-now-route-matches'];
+
 	res.writeHead(200);
-	res.end(`isr: ${new Date().toISOString()} ${req.method} ${req.url}`);
+	res.end(`isr: ${new Date().toISOString()} ${req.method} ${req.url} ${url}`);
 };
